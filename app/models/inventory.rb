70b0a03b9833f20acbaec67155b606c98441ca06
Oscar Abrego <oscara99@hotmail.com>
2016-10-27 20:44:31 -0500

    Third commit

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,12 @@
 class Inventory < ActiveRecord::Base
   belongs_to :category
+
+  validate :valid_size
+
+
+  def valid_size
+    if self.size.nil? || self.size <= 0
+      errors.add(:size, 'is an imposible value, lower than zero')
+    end
+  end
 end
