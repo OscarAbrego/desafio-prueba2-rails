bacb2c1dffc560df0a3fbb1ce4d60344955c6bbe
Oscar Abrego <oscara99@hotmail.com>
2016-10-27 22:25:17 -0500

    Ninth commit

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,7 +1,10 @@
 class Inventory < ActiveRecord::Base
   belongs_to :category
-  has_one :work, dependent: :delete_all
+  has_one :work, dependent: :delete
   validate :valid_size
+
+  after_destroy :log_destroy
+
 
   scope :size_x, -> (x) {
     where(:size => x)
@@ -12,4 +15,11 @@ class Inventory < ActiveRecord::Base
       errors.add(:size, 'is an imposible value,  lower than zero')
     end
   end
+
+  private
+
+  def log_destroy
+    Log.create(description:"La pieza con serial #{self.serial} ah sido borrada")
+  end
+
 end
